7e9b37c91d283a6d6cb7212013594129b3c9d127
TitanTechnos <titantechnos@gmail.com>
2018-01-24 17:00:00 -0700

    Stage 4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -52,7 +52,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="TeleOP", group="Iterative Opmode")
+@TeleOp(name="TeleOP")
 
 public class TeleOP extends OpMode
 {
